d499208d6046d9e9e4181fb48f70f209d700b4a9
GGgaijin <melirich1998@gmail.com>
2025-02-06 02:17:57 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,1 +1,8 @@
+<?php
+function sendEmail($to, $subject, $message) {
+    $headers = "From: no-reply@e-leave-system.com\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
+    mail($to, $subject, $message, $headers);
+}
+?>
